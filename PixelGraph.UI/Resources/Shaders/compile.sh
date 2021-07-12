3b0bcaf535197334c382664527b16752d0132685
NULL511 <null511@outlook.com>
2021-07-12 14:42:29 -0400

    fix shader build order; release 1.5.2

--- PixelGraph.UI/Resources/Shaders/compile.sh
+++ PixelGraph.UI/Resources/Shaders/compile.sh
@@ -1,4 +1,4 @@
-Echo Compiling shader HLSL to CSO
+echo Compiling shader HLSL to CSO
 cd "%~dp0"
 PATH="c:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Utilities\bin\x86\"
 fxc /T ps_5_0 /E PSMain "psBasic.hlsl" /Fo "psBasic.cso"
