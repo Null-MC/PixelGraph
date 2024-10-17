12f51ba74d952e5a8c2bfc0360305bc2fe9eaf80
NULL511 <null511@outlook.com>
2024-10-16 23:19:51 -0400

    release inno shaders fix

--- Inno/publish.sh
+++ Inno/publish.sh
@@ -1,2 +1,3 @@
+dotnet build PixelGraph.Rendering -c Release
 dotnet publish ..\\PixelGraph.UI -c Release -r win-x64 -o src -p:EnableWindowsTargeting=true --self-contained false
 "C:\\Program Files (x86)\\Inno Setup 6\\ISCC.exe" "setup.iss"
